e3663ff8edcadd2befaa0f2ba4ee8b6f2dc332b8
tanvirgeek <tanvirgeek@gmail.comm>
2019-09-12 21:02:34 +0600

    initial commit

---
+++ precache-manifest.36e7e9a75f70523cb1d4e5a7435d77e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a20099a5551e365ca4b4a9f9f89721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d90d75e2f14fa5fadbc4",
+    "url": "/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "6afc3dbefcdca98187f1",
+    "url": "/static/js/2.a032950e.chunk.js"
+  },
+  {
+    "revision": "d90d75e2f14fa5fadbc4",
+    "url": "/static/js/main.d6b797e1.chunk.js"
+  },
+  {
+    "revision": "a4592f19f370e6d01b52",
+    "url": "/static/js/runtime~main.0f62063b.js"
+  }
+]);
